22d721a7af34341e036735091da17eeaa02a26a4
ivobul <24499940+ivobul@users.noreply.github.com>
2022-12-26 09:51:56 -0500

    Add files via upload

--- web-developer-portfolio/script.js
+++ web-developer-portfolio/script.js
@@ -1,6 +1,13 @@
 const hamburger = document.querySelector(".page-header__hamburger");
+const hamburgerIcon = hamburger.querySelector(".material-icons")
 const nav = document.querySelector(".page-header__nav");
 
 hamburger.addEventListener("click", () => {
+  if (hamburgerIcon.innerText === "menu")
+    hamburgerIcon.innerText = "close"
+  else {
+    hamburgerIcon.innerText = "menu"
+  }
+  
   nav.classList.toggle("active")
 })
