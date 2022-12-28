656f22853a51432b31f76d07b538765f89885cfd
ivobul <24499940+ivobul@users.noreply.github.com>
2022-12-28 12:12:25 -0500

    Add files via upload

--- unifeed-blog-template/script.js
+++ unifeed-blog-template/script.js
@@ -1,6 +1,14 @@
-const mobileMenu = document.querySelector(".site-header__mobile");
-const mainNav = document.querySelector(".main-nav");
+const hamburger = document.querySelector(".site-header__hamburger");
+const hamburgerIcon = hamburger.querySelector(".material-icons");
+const nav = document.querySelector(".main-nav");
 
-mobileMenu.addEventListener("click", () => {
-  mainNav.classList.toggle("active")
-})
+hamburger.addEventListener("click", () => {
+  if (hamburgerIcon.innerText === "menu") {
+    hamburgerIcon.innerText = "close";
+  }
+  else {
+    hamburgerIcon.innerText = "menu";
+  }
+  
+  nav.classList.toggle("active");
+});
