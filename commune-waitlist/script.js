2c17fb2f1b42d8ee687aad7c513a1d7d13be98e0
ivobul <24499940+ivobul@users.noreply.github.com>
2024-10-11 15:12:12 -0400

    Add files via upload

--- commune-waitlist/script.js
+++ commune-waitlist/script.js
@@ -1,14 +1,33 @@
-const hamburger = document.querySelector(".page-header__hamburger");
-const hamburgerIcon = hamburger.querySelector(".material-icons");
-const nav = document.querySelector(".page-header__nav");
+const hamburger = document.querySelector(".page-header__hamburger")
+const hamburgerIcon = hamburger.querySelector(".material-icons")
+const nav = document.querySelector(".page-header__nav")
+const validation = new JustValidate(".page-main__form")
 
 hamburger.addEventListener("click", () => {
   if (hamburgerIcon.innerText === "menu") {
-    hamburgerIcon.innerText = "close";
-  }
-  else {
-    hamburgerIcon.innerText = "menu";
+    hamburgerIcon.innerText = "close"
   }
   
-  nav.classList.toggle("active");
-});
+  else {
+    hamburgerIcon.innerText = "menu"
+  }
+  
+  nav.classList.toggle("active")
+})
+
+
+
+validation
+  .addField("#email", [
+    {
+      rule: "required",
+      errorMessage: "Email cannot be empty",
+    },
+    {
+      rule: "email",
+      errorMessage: "Looks like this is not an email",
+    },
+  ])
+  .onSuccess(() => {
+    document.querySelector(".page-main__form").submit()
+  })
