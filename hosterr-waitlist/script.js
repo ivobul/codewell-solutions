b23612d5a5919a119b03e3712eaa1d21083087b9
ivobul <24499940+ivobul@users.noreply.github.com>
2024-10-13 10:03:43 -0400

    Add files via upload

--- hosterr-waitlist/script.js
+++ hosterr-waitlist/script.js
@@ -5,10 +5,12 @@ const closeIconElem = document.querySelector(".mobile-nav__close-icon")
 
 hamburgerIconElem.addEventListener("click", () => {
   mobileNavElem.classList.add("active")
+  mobileNavElem.style.transition = "left .8s"
 })
 
 closeIconElem.addEventListener("click", () => {
   mobileNavElem.classList.remove("active")
+  mobileNavElem.style.transition = "left 1s"
 })
 
 
