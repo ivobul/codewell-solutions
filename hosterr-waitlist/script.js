01afe3777e00648deb8938fd1ca26232e9a3f204
ivobul <24499940+ivobul@users.noreply.github.com>
2024-10-13 10:17:12 -0400

    Add files via upload

--- hosterr-waitlist/script.js
+++ hosterr-waitlist/script.js
@@ -1,7 +1,6 @@
 const hamburgerIconElem = document.querySelector(".page-header__hamburger-icon")
 const mobileNavElem = document.querySelector(".mobile-nav")
 const closeIconElem = document.querySelector(".mobile-nav__close-icon")
-//const secondaryNavElem = document.querySelector(".page-header__right")
 
 hamburgerIconElem.addEventListener("click", () => {
   mobileNavElem.classList.add("active")
