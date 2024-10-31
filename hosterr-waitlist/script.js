b428b2f187a8be86972712b7e545753055090c3c
ivobul <24499940+ivobul@users.noreply.github.com>
2024-10-31 10:10:41 -0400

    Add files via upload

--- hosterr-waitlist/script.js
+++ hosterr-waitlist/script.js
@@ -2,20 +2,24 @@ const hamburgerIconElem = document.querySelector(".page-header__hamburger-icon")
 const mobileNavElem = document.querySelector(".mobile-nav")
 const overlayElem = document.querySelector(".overlay")
 const closeIconElem = document.querySelector(".mobile-nav__close-icon")
+const rootElem = document.querySelector("html")
 
 hamburgerIconElem.addEventListener("click", () => {
   mobileNavElem.classList.add("active")
   overlayElem.style.display = "block"
+  rootElem.style.overflowY = "hidden"
 })
 
 closeIconElem.addEventListener("click", () => {
   mobileNavElem.classList.remove("active")
   overlayElem.style.display = "none"
+  rootElem.style.overflowY = "auto"
 })
 
 overlayElem.addEventListener("click", () => {
   overlayElem.style.display = "none"
   mobileNavElem.classList.remove("active")
+  rootElem.style.overflowY = "auto"
 })
 
 // Form Validation
